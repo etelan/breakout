f49c57cca1f6f1b60ad435541a8e720129bbd7d6
etelan <adamjamesbaker@gmail.com>
2020-03-07 12:34:16 +0000

    detect key press
    Left and Right keys.

--- module/code.py
+++ module/code.py
@@ -1,3 +1,7 @@
+#TODO: Detect left and right keys <-
+#TODO: Add character
+#TODO: Add blocks to hit with ball.
+
 #Pygame library for games.
 import pygame
 
@@ -14,16 +18,30 @@ pygame.display.set_caption("This Is A Caption")
 
 #Set Background Colour
 red = [255,0,0]
-screen.fill(red)
+white = [255,255,255]
+screen.fill(white)
 pygame.display.update()
 
 #Tell the game to run
 running = True
 
-#While it is running, do something else.
+#Main Loop
 while(running):
+    
     #Get all the user events at each frame
     for event in pygame.event.get():
+
+        #Key presses.
+        if event.type == pygame.KEYDOWN:
+            
+            #Left
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                print("left detected")
+            #Right
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                print("right detected")
+        
+        
         #If the event is a quit, quit.
         if event.type == pygame.QUIT:
             #Set running to false, as we want it to quit.
